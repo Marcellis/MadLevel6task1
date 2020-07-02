9568ead8431763356e3d5dfdfdebb64980d03572
Pim Meijer <p.meijer@hva.nl>
2020-07-02 16:09:55 +0200

    forgotten encapsulation for livedata

--- app/src/main/java/nl/hva/level6task1/vm/ColorViewModel.kt
+++ app/src/main/java/nl/hva/level6task1/vm/ColorViewModel.kt
@@ -1,5 +1,6 @@
 package nl.hva.level6task1.vm
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import nl.hva.level6task1.model.ColorItem
@@ -8,7 +9,11 @@ import nl.hva.level6task1.repository.ColorRepository
 class ColorViewModel : ViewModel(){
     private val colorRepository = ColorRepository()
 
-    val colorItems = MutableLiveData<List<ColorItem>>().apply {
+    //use encapsulation to expose as LiveData
+    val colorItems: LiveData<List<ColorItem>>
+        get() = _colorItems
+
+    private val _colorItems = MutableLiveData<List<ColorItem>>().apply {
         value = colorRepository.getColorItems()
     }
 }
